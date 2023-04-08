ee3340f9460e5e576d25141e7b2a64e61054eddb
dashk <dashk@X>
2023-04-08 16:30:27 +0300

    Добавьте файлы проекта.

---
+++ paint/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(738, 562);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(816, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(750, 72);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(181, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(744, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 116);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tools";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ellipse";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButtons_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(109, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Rectangle";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButtons_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Pen";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButtons_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(744, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(744, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 56);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Load Img";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(744, 493);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 56);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Save Img";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(750, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(58, 53);
+            this.panel1.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(937, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ paint/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = pen;
+            radioButton2.Checked = rectangle;
+            radioButton3.Checked = ellipse;
+            panel1.BackColor = myPen.Color;
+        }
+
+        bool pen = true;
+        bool rectangle = false;
+        bool ellipse = false;
+
+        static SolidBrush brush = new SolidBrush(Color.Black);
+        Pen myPen = new Pen(brush,1);
+
+        Point startPoint;
+        bool isMouseDown = false;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            startPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                if(startPoint != null)
+                {
+                    if (pen)
+                    {
+                        if (pictureBox1.Image == null)
+                        {
+                            Bitmap tempBmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                            pictureBox1.Image = tempBmp;    
+
+                        }
+                        using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                        {
+                           if(e.Button == MouseButtons.Left)
+                           {
+                                myPen.Color = panel1.BackColor;
+                           }
+
+                           else if (e.Button == MouseButtons.Right)
+                           {
+                                myPen.Color = Color.White;
+                           }
+                            g.DrawLine(myPen, startPoint, e.Location);
+                            pictureBox1.Invalidate();
+                        }
+                    }
+                }
+            }
+            if (pen)
+            {
+                startPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (rectangle || ellipse)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    Bitmap tempBmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = tempBmp;
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    if(rectangle)
+                    {
+                        if(e.Location.X > startPoint.X && e.Location.Y > startPoint.Y)
+                        {
+                            g.DrawRectangle(myPen, startPoint.X,startPoint.Y,e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+                        }
+                    }
+                    if (ellipse)
+                    {
+                        g.DrawEllipse(myPen, startPoint.X, startPoint.Y, e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+                    }
+                }
+                pictureBox1.Invalidate();
+            }
+            startPoint = Point.Empty;
+            isMouseDown = false;    
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            myPen.Width = (float)numericUpDown1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myPen.Color = colorDialog1.Color;
+                panel1.BackColor = colorDialog1.Color; 
+
+            }
+
+        }
+        private void ChangeInstrument()
+        {
+            if(radioButton1.Checked)
+            {
+                pen = true;
+                rectangle = false;
+                ellipse = false;
+            }
+            if (radioButton2.Checked)
+            {
+                pen = false;
+                rectangle = true;
+                ellipse = false;
+            }
+            if(radioButton3.Checked)
+            {
+                pen = false;
+                rectangle = false;
+                ellipse = true;
+            }
+        }
+
+        private void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeInstrument();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "MyImg";
+            saveFileDialog1.DefaultExt = ".jpg";
+            saveFileDialog1.Filter = "JPEG FILES(*.jpg)|*.jpg|All files(*.*)|*.*";
+            saveFileDialog1.Title = "Saving your Img......";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                Bitmap buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.DrawToBitmap(buffer, new Rectangle(0,0,pictureBox1.Width,pictureBox1.Height));
+                buffer.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".jpg";
+            openFileDialog1.Filter = "JPEG FILES(*.jpg)|*.jpg|All files(*.*)|*.*";
+            openFileDialog1.Title = "Open your Img......";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Bitmap openimage = new Bitmap(openFileDialog1.FileName);
+                if(pictureBox1.Image == null)
+                {
+                    pictureBox1.Image = bmp;
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+
+                 g.DrawImage(openimage, 0, 0 ,pictureBox1.Width, pictureBox1.Height);
+
+                }  
+            }
+        }
+    }
+}
